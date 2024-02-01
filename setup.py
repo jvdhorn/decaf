c3a504699ba30c52bbb0a2c931d8a83fee874bde
Jitse van der Horn <horn@ala.chem.uu.nl>
2024-02-01 14:42:25 +0100

    Updated package description

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages, findall
 
 setup(  name                = 'diffuse',
         version             = '0.1',
-        description         = 'A Python package for simulating diffuse scattering of X-rays in protein crystals',
+        description         = 'A Python package for extracting and simulating diffuse scattering of X-rays in protein crystals',
         author              = 'J A van der Horn',
         author_email        = 'jitsie@gmail.com',
         url                 = '',
