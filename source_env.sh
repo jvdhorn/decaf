d5b2003f587e0bac997ed12cb4890864164cdf54
jvdhorn <jitsie@gmail.com>
2025-07-03 13:12:41 +0200

    Updated source_env.sh to allow sourcing from outside directory

--- source_env.sh
+++ source_env.sh
@@ -1,4 +1,4 @@
-DIR=$(realpath "$(dirname "${BASH_SOURCE[0]}")")
+DIR=$(realpath "$(dirname "${BASH_SOURCE[0]:-$0}")")
 export PYTHONPATH=${PYTHONPATH}:${DIR}
 export PATH=${PATH}:${DIR}/bin
 export PHENIX_TRUST_OTHER_ENV=1
