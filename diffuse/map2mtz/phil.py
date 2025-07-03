82330ebf98df85cbb04706b67a30c7df23258759
jvdhorn <jitsie@gmail.com>
2025-07-03 13:12:39 +0200

    Included map2mtz utility for experimental reconstructions

---
+++ diffuse/map2mtz/phil.py
@@ -0,0 +1,45 @@
+import libtbx.phil
+
+def phil_parse(args=None):
+  '''
+  Contains default parameters, will process commandline params and 
+  changes them
+  '''
+  # Default parameters
+  master_phil = libtbx.phil.parse("""
+  map2mtz
+    {
+      input
+        .help = "Input files"
+      {
+        map = None
+          .type = path
+          .help = 'Map input file'
+        resolution = 0
+          .type = floats
+          .help = 'Resolution range (or max if one value provided)'
+        cutoff = None
+          .type = float
+          .help = 'Cutoff value for valid intensities (minimum is used if nothing is provided)'
+      }
+      output
+        .help = "Output files"
+      {
+        lbl = 'IDFF'
+          .type = str
+          .help = 'Output file array'
+        mtz_out = None
+          .type = path
+          .help = 'Mtz output file'
+      }
+    }
+    """)
+
+  for i, arg in enumerate(args):
+    if '=' not in arg:
+      args[i] = 'map='+arg
+  interpreter = master_phil.command_line_argument_interpreter()
+  arguments = [interpreter.process(arg) for arg in args]
+  working_phil = master_phil.fetch(sources = arguments)
+
+  return working_phil
